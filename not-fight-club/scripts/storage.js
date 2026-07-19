08e65849d272448208fce6e391371342b8ec32bb
Svetajism <sv.zacharova@gmail.com>
2026-07-19 20:16:16 +0200

    feat: add localStorage function

--- not-fight-club/scripts/storage.js
+++ not-fight-club/scripts/storage.js
@@ -1,1 +1,8 @@
-// saveState/loadState (JSON вокруг localStorage, один ключ)
+export function saveState(state) {
+    localStorage.setItem('nfc_state', JSON.stringify(state));
+}
+
+export function loadState() {
+    const stateJSON = localStorage.getItem('nfc_state');
+    return stateJSON ? JSON.parse(stateJSON) : null;
+}
